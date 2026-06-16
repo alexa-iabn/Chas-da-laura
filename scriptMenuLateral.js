8aacceeb33a0ae11ecb3f3ddb7f8b833ac441177
alexa <alexandra.barbosa1103@gmail.com>
2026-06-15 21:56:53 -0300

    novos arquivos panel lateral

---
+++ scriptMenuLateral.js
@@ -0,0 +1,20 @@
+/* ABRIR E FECHAR O MENU LATERAL */
+   
+   const btnSidebar = document.querySelector("#botaoRetratil");
+
+    btnSidebar.addEventListener("click", () => {
+        document.body.classList.toggle("sidebarFechado");
+    });
+
+
+    const itensMenu = document.querySelectorAll(".blocoOpcao");
+
+    itensMenu.forEach((item) => {
+        item.addEventListener("click", () => {
+            itensMenu.forEach((outroItem) => {
+                outroItem.classList.remove("ativo");
+            });
+
+            item.classList.add("ativo");
+        });
+    });
